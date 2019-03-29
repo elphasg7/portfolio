6c1fc31a2672eceeebd602475ee50c76b4fccdb6
elphasg7 <47386899+elphasg7@users.noreply.github.com>
2019-03-29 10:05:07 +0200

    Add files via upload

---
+++ Beginner_Java/Soccer/Goal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice13_3;
+
+/**
+ *
+ * @author Elphas
+ */
+public class Goal extends GameEvent {
+    public String toString(){
+        return "Goal scored";
+    }
+}
---
+++ Beginner_Java/Soccer/Player.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice13_3;
+
+/**
+ *
+ * @author Elphas
+ */
+public class Player {
+     private String playerName;
+     private int goalsScored;
+    
+     public void  incGoalsScored(){
+     this.goalsScored++;
+     }
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+    
+    public Player() {}
+
+    /**
+     * @return the playerName
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    /**
+     * @param playerName the playerName to set
+     */
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    /**
+     * @return the goalsScored
+     */
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    /**
+     * @param goalsScored the goalsScored to set
+     */
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+    
+}
